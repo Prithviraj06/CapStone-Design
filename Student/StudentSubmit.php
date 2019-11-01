e438a3e3a7ccb5a4ab4a4de00357efef1af19b39
Prithviraj Naidu <49693008+Prithviraj06@users.noreply.github.com>
2019-10-31 18:28:44 -0700

    Update StudentSubmit.php

--- Student/StudentSubmit.php
+++ Student/StudentSubmit.php
@@ -11,17 +11,21 @@ $CumGpa2 = $_POST['inputcumGPA'];
 $InProgressUnits2 = $_POST['inputprogressunits'];
 $CumUnits2 = $_POST['inputcumunits'];
 $Comments2 = $_POST['inputAdditional'];
+    
+    echo "The name of the student is ".$fname2." and the last name  is ".$lname2." with email id ".$email2." and permanent email ".$Peremail2.". The Grad term of the student is ".$GradTerm2." with the admit term being ".$AdmitTerm2.". The term GPA of the student is ".$TermGpa2." and the cumulative GPA is ".$CumGpa2.". The in Progress Units of the students is ".$InProgressUnits2." and the total units completed by the student is ".$CumUnits2.". My additional comments for the student is '".$Comments2."'.";
+    
+    $con = mysqli_connect("localhost", "root", "Prna.123!","testdb");
 
-$conn = mysqli_connect("localhost", "root", " ","testdb");
-$query = "insert into studentrec(fname,lname, email, Peremail, GradTerm,AdmitTerm, TermGpa,CumGpa,InProgressUnits,CumUnits,Comments) values ('hi','$lname2','$email2','$Peremail2','$GradTerm2','$AdmitTerm2','$TermGpa2','$CumGpa2','$InProgressUnits2','$CumUnits2','$Comments2')"; //Insert Query
+    if(mysqli_connect_errno())
+    {
+          echo "Failed to connect to MySQL: ".mysqli_connect_errno();
+    }
+    
+    $sql = "INSERT INTO studentrec(fname,lname, email, Peremail, GradTerm,AdmitTerm, TermGpa,CumGpa,InProgressUnits,CumUnits,Comments) VALUES('$fname2','$lname2','$email2','$Peremail2','$GradTerm2','$AdmitTerm2','$TermGpa2','$CumGpa2','$InProgressUnits2','$CumUnits2','$Comments2')"; //Insert Query
 
   
-    
-$result = mysqli_query($conn,$query);
-
-
-
-mysqli_close($result); // Connection Closed
-    header('Location: index.html');
+    $result = mysqli_query($con,$sql);
+   
+  header('Location: index.html');
 }
 ?>
